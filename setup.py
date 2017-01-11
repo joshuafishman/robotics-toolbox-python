54f272020d5c0c41c2a3a728a0324ee81795ed52
joshua fishman <joshuafishman1@gmail.com>
2017-01-11 11:07:37 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(name='Robotics-Toolbox-Python',
+      description='Robotics Toolbox for Python',
+      author='Peter Corke',
+      url='https://github.com/petercorke/robotics-toolbox-python',
+      packages=['robot'],
+     )
